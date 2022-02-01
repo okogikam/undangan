1dc7841cd923efb2689538286e563d38fc695a50
okogikam <okogikam@gmail.com>
2022-02-01 14:06:08 +0800

    20220201

---
+++ scrip.js
@@ -0,0 +1,8 @@
+let btnsubmit = document.querySelector(".submit");
+
+btnsubmit.addEventListener("click", () => {
+  event.preventDefault();
+  let name = document.querySelector(".nama");
+  let pesan = document.querySelector(".pesan");
+  let hadir = document.querySelector(".hadir");
+});
